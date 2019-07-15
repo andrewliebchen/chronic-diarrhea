21f37be1c973b1e427f93deac391860eb3bb7781
Andrew Liebchen <andrewliebchen@gmail.com>
2019-07-14 22:36:17 -0700

    Fix scrolling

--- src/App.js
+++ src/App.js
@@ -16,13 +16,6 @@ const initialLines = [
   "Chronic Diarrhea"
 ];
 
-const Wrapper = styled(Box)`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-`;
-
 const Even = styled(Text)`
   font-family: "Rye", cursive;
   font-size: 2em;
@@ -39,35 +32,33 @@ function App() {
   const [lines, setLines] = useState(initialLines);
 
   return (
-    <Wrapper p={3} width="75vw">
-      <Flex>
-        <Box width={1 / 4} mr={6}>
-          {lines.map((line, i) => (
-            <input
-              key={i}
-              type="text"
-              defaultValue={line}
-              onChange={event => {
-                let newLines = lines;
-                newLines.splice(i, 1, event.target.value);
-                setLines([...newLines]);
-              }}
-            />
-          ))}
-        </Box>
-        <TShirt width={1 / 2} p={5} pt={6}>
-          {lines.map((line, i) => (
-            <Box>
-              {i % 2 === 0 ? (
-                <Even textAlign="center">{line}</Even>
-              ) : (
-                <Odd textAlign="center">{line}</Odd>
-              )}
-            </Box>
-          ))}
-        </TShirt>
-      </Flex>
-    </Wrapper>
+    <Flex p={3}>
+      <Box width={1 / 4} mr={6}>
+        {lines.map((line, i) => (
+          <input
+            key={i}
+            type="text"
+            defaultValue={line}
+            onChange={event => {
+              let newLines = lines;
+              newLines.splice(i, 1, event.target.value);
+              setLines([...newLines]);
+            }}
+          />
+        ))}
+      </Box>
+      <TShirt width={1 / 2} p={5} pt={6}>
+        {lines.map((line, i) => (
+          <Box>
+            {i % 2 === 0 ? (
+              <Even textAlign="center">{line}</Even>
+            ) : (
+              <Odd textAlign="center">{line}</Odd>
+            )}
+          </Box>
+        ))}
+      </TShirt>
+    </Flex>
   );
 }
 
