09da091a138014ced388e8ca17dbff907c7a7006
Andrew Liebchen <andrewliebchen@gmail.com>
2019-07-14 18:02:12 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Box, Flex } from "rebass";
+
+const initialLines = [
+  "I was a theater kid",
+  "and now I'm a",
+  "designer",
+  "who's older than my",
+  "coworkers",
+  "and they don't totally get my",
+  "jokes",
+  "but people like my",
+  "cat pictures on",
+  "Instagram"
+];
+
+const Wrapper = styled(Box)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+`;
+
+const Text = styled.div`
+  text-align: center;
+`;
+
+const Odd = styled(Text)`
+  font-family: "Vast Shadow", cursive;
+  font-size: 2em;
+`;
+
+const Even = styled(Text)`
+  font-family: "Bangers", cursive;
+  font-size: 4em;
+`;
 
 function App() {
+  const [lines, setLines] = useState(initialLines);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper p={3} width="75vw">
+      {lines.map((line, i) => (
+        <Flex key={i}>
+          <Box width={3 / 4} pr={3}>
+            {i % 2 === 0 ? <Even>{line}</Even> : <Odd>{line}</Odd>}
+          </Box>
+          <Box width={1 / 4}>
+            <input
+              type="text"
+              defaultValue={line}
+              onChange={event => {
+                let newLines = lines;
+                newLines.splice(i, 1, event.target.value);
+                setLines([...newLines]);
+              }}
+            />
+          </Box>
+        </Flex>
+      ))}
+    </Wrapper>
   );
 }
 
